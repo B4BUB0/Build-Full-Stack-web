88ee9244cb13e24b83c6fec3ff6d1676c3878d62
BRAVEcrash <baiyshali39@gmail.com>
2026-01-02 17:49:56 +0900

    movie-app

--- full-stack-web/src/App.jsx
+++ full-stack-web/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react" 
+import Search from './components/Search.jsx'
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState( initialState='I am Batman');
   return (
-    <h2>Functional Arrow Component</h2>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper" />
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+
+          <h1>Find <span 
+className="text-gradient">Movies </span> You'll Enjoy 
+Without the Hassle</h1>
+        </header>
+        
+        <Search searchTerm={searchTerm}setSearchTerm={setSearchTerm} />
+      <div/>
+    </main>
+    
+     
   )
 
 }
