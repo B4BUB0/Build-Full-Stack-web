8be614f5b73d52aa6574d006b99e5d7458d93fc5
BRAVEcrash <baiyshali39@gmail.com>
2026-01-13 15:55:42 +0900

    trend

--- full-stack-web/src/App.jsx
+++ full-stack-web/src/App.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import Search from './components/Search.jsx'
 import MovieCard from './components/MovieCard.jsx';
 import {useDebounce} from 'react-use'
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -53,6 +54,10 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch(error) {
       console.error('Error fetching movies: Please try again later');
     } finally {
